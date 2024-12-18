a7670f8ea24c4a0d5903cb199a9286039622bd66
Shubhankar Mitra <shubhankar.mitra@hoichoi.tv>
2024-12-18 11:14:59 +0530

    base url changes

--- frontend/src/services/LoanService.js
+++ frontend/src/services/LoanService.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/loans";
+const BASE_URL = import.meta.PROD
+  ? "https://techdome-oitw.onrender.com"
+  : "http://localhost:5000";
+
+const API_BASE_URL = `${BASE_URL}/api/loans`;
 
 export const createLoan = async (userId, amount, term) => {
   return await axios.post(API_BASE_URL, { userId, amount, term });
